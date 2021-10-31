f828deeb9ec6400af333f2a07253701771b26c6f
thread-koder <mohamad.fberjawi@gmail.com>
2021-10-31 15:41:37 +0200

    Add global css file with the fonts settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~layouts/global.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
