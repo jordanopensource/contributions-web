92d75d44f67e207f607e2af93fad6072558d9dac
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-02 19:12:14 +0200

    Add a button to toggle the Filter section

--- components/FilterSection.vue
+++ components/FilterSection.vue
@@ -3,42 +3,51 @@
     <div class="content-conatiner">
       <div class="filter-section">
         <h1 class="filter-text">Filter</h1>
+        <button class="outline-none" @click="ToggleIsOpen">
+          <img
+            class="w-6 h-6"
+            src="~/assets/images/filter/icon-small-chevron.svg"
+            alt=""
+          />
+        </button>
       </div>
-      <div class="sort-section has-border">
-        <h6 class="text-xs font-bold">Sort by:</h6>
-        <div class="input-section"></div>
-        <label class="text-xs">
-          <input type="radio" />
-          Score
-        </label>
-        <label class="text-xs px-10">
-          <input type="radio" />
-          Contributions
-        </label>
-      </div>
-      <div class="sort-section has-border">
-        <h6 class="text-xs font-bold">Show:</h6>
-        <div class="input-section"></div>
-        <label class="text-xs">
-          <input type="radio" />
-          All
-        </label>
-        <label class="text-xs px-14">
-          <input type="radio" />
-          Only JOSA members
-        </label>
-      </div>
-      <div class="sort-section">
-        <h6 class="text-xs font-bold">Count:</h6>
-        <div class="input-section"></div>
-        <label class="text-xs">
-          <input type="checkbox" />
-          Commits
-        </label>
-        <label class="text-xs px-5">
-          <input type="checkbox" />
-          Contribution
-        </label>
+      <div :class="isOpen ? '' : 'hidden overflow-hidden'">
+        <div class="sort-section has-border">
+          <h6 class="text-xs font-bold">Sort by:</h6>
+          <div class="input-section"></div>
+          <label class="text-xs">
+            <input type="radio" />
+            Score
+          </label>
+          <label class="text-xs px-10">
+            <input type="radio" />
+            Contributions
+          </label>
+        </div>
+        <div class="sort-section has-border">
+          <h6 class="text-xs font-bold">Show:</h6>
+          <div class="input-section"></div>
+          <label class="text-xs">
+            <input type="radio" />
+            All
+          </label>
+          <label class="text-xs px-14">
+            <input type="radio" />
+            Only JOSA members
+          </label>
+        </div>
+        <div class="sort-section">
+          <h6 class="text-xs font-bold">Count:</h6>
+          <div class="input-section"></div>
+          <label class="text-xs">
+            <input type="checkbox" />
+            Commits
+          </label>
+          <label class="text-xs px-5">
+            <input type="checkbox" />
+            Contribution
+          </label>
+        </div>
       </div>
     </div>
   </div>
@@ -47,6 +56,20 @@
 <script>
 export default {
   name: 'FilterSection',
+  data() {
+    return {
+      isOpen: true,
+    }
+  },
+  methods: {
+    ToggleIsOpen() {
+      if (this.isOpen) {
+        this.isOpen = false
+      } else {
+        this.isOpen = true
+      }
+    },
+  },
 }
 </script>
 
@@ -57,11 +80,11 @@ export default {
 
 .content-conatiner {
   background-color: #f4f4f5;
-  @apply mx-3 mt-5;
+  @apply mx-3;
 }
 
 .filter-section {
-  @apply flex mx-6 pt-6 pb-1 border-b-2 border-dotted border-black;
+  @apply flex mx-6 pt-6 pb-2 border-b-2 border-dotted border-black justify-between;
 }
 
 .filter-text {
