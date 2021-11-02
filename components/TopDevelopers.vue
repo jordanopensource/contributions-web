99b979d24ddd2da580692d7d34e18ad29f50320b
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-02 17:17:59 +0200

    Create the Top Developers Component

---
+++ components/TopDevelopers.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <article class="section-container">
+      <div class="header-title">
+        <div class="divider"></div>
+        <h1>Top Developers in Jordan</h1>
+      </div>
+      <FilterSection />
+    </article>
+  </div>
+</template>
+
+<script>
+import FilterSection from './FilterSection.vue'
+export default {
+  name: 'TopDevelopers',
+  components: {
+    FilterSection,
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.section-container {
+  background-color: #edeeef;
+}
+.divider {
+  @apply w-10 lg:w-14 border-t-2 border-black;
+}
+.header-title {
+  font-family: 'IBM Mono';
+  font-size: 1.7rem;
+  @apply font-normal ml-8 pt-16 md:ml-12 lg:ml-20 lg:text-4xl lg:font-light 2xl:ml-24;
+}
+</style>
