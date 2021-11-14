21d98036ee2d66bd19891b26746e52d494eafe04
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-14 16:55:59 +0200

    Make the sort by query parameter dynamic

--- components/PaginationBar.vue
+++ components/PaginationBar.vue
@@ -44,6 +44,9 @@ export default {
     currentPage() {
       return this.$store.getters.getCurrentPage
     },
+    sortBy() {
+      return this.$store.getters.getSortBy
+    },
   },
   methods: {
     async fetchCurrentPage(page) {
@@ -52,7 +55,7 @@ export default {
         this.limitNumberWithinRange(page, 1, this.pageCount)
       )
       const response = await this.$axios.get(
-        `http://localhost:8080/api/v1/users?page=${this.currentPage}&sort_by=score`
+        `http://localhost:8080/api/v1/users?page=${this.currentPage}&sort_by=${this.sortBy}`
       )
       this.$store.commit('setUsers', response.data.users.docs)
     },
