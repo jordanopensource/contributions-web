591effdc67366b3c4a94a2a7c5d260a6a3211ea7
thread-koder <mohamad.fberjawi@gmail.com>
2021-11-21 14:25:53 +0200

    Change the title of the web page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'top-contributors',
+    title: 'Jordan’s Top Contributors',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
