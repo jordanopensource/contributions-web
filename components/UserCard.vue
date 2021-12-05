473ad10ab5c5a82073352f14adfa666f0a444359
Mohamad Berjawi <mohamad.fberjawi@gmail.com>
2021-12-05 15:56:57 +0200

    Remove the lazy loading from the user card image

--- components/UserCard.vue
+++ components/UserCard.vue
@@ -2,14 +2,7 @@
   <div class="flex mx-8 h-32 lg:h-40 content-center has-border pb-3">
     <h1 class="rank">{{ rank }}.</h1>
     <div class="image-container">
-      <img
-        class="image"
-        :src="imageUrl"
-        alt="avatar"
-        width="80"
-        height="80"
-        loading="lazy"
-      />
+      <img class="image" :src="imageUrl" alt="avatar" width="80" height="80" />
       <span class="image-filter"></span>
     </div>
 
