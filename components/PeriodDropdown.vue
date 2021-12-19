ba47568dd2b992829c8aadb7b32b965240c59591
thread-koder <mohamad.fberjawi@gmail.com>
2021-12-19 15:56:42 +0200

    Make dates ranges change dynamically based on the option

--- components/PeriodDropdown.vue
+++ components/PeriodDropdown.vue
@@ -3,15 +3,22 @@
     <select id="period" name="period" @change="onChange">
       <option value="last30Days" selected>Last 30 Days</option>
       <option value="lastMonth">Last Month</option>
-      <option value="pastYear">Past Year</option>
+      <option value="thisYear">This Year</option>
     </select>
-    <p>
+    <p v-if="period === 'last30Days'">
       <small>{{ last30Days }} ➜ {{ todayDate }}</small>
+    </p>
+    <p v-if="period === 'lastMonth'">
+      <small>{{ firstDayOfTheLastMonth }} ➜ {{ lastDayOfTheLastMonth }}</small>
+    </p>
+    <p v-if="period === 'thisYear'">
+      <small>{{ startOfTheYear }} ➜ {{ todayDate }}</small>
     </p>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'PeriodDropdown',
   data() {
@@ -20,13 +27,41 @@ export default {
         day: 'numeric',
         month: 'long',
       }),
+      startOfTheYear: new Date(new Date().getFullYear(), 0, 1).toLocaleString(
+        'en-GB',
+        {
+          day: 'numeric',
+          month: 'long',
+        }
+      ),
+      firstDayOfTheLastMonth: 0,
+      lastDayOfTheLastMonth: new Date(
+        new Date().getFullYear(),
+        new Date().getMonth(),
+        0
+      ).toLocaleString('en-GB', {
+        day: 'numeric',
+        month: 'long',
+      }),
       last30Days: 0,
     }
+  },
+  computed: {
+    ...mapState({
+      period: 'period',
+    }),
   },
   mounted() {
     const today = new Date()
     const priorDate = new Date().setDate(today.getDate() - 30)
     this.last30Days = new Date(priorDate).toLocaleString('en-GB', {
+      day: 'numeric',
+      month: 'long',
+    })
+    const firstDayOfLastMonth = new Date(today)
+    firstDayOfLastMonth.setDate(1)
+    firstDayOfLastMonth.setMonth(today.getMonth() - 1)
+    this.firstDayOfTheLastMonth = firstDayOfLastMonth.toLocaleString('en-GB', {
       day: 'numeric',
       month: 'long',
     })
