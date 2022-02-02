c164e6191e2814b82ff979e02842eaeda9fd71ca
thread-koder <mohamad.fberjawi@gmail.com>
2022-02-02 17:54:52 +0200

    Add the vue-apexcharts plugin to the nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   css: ['~layouts/global.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/vue-apexchart.js', ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
