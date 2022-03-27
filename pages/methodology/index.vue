54b0568d8449617eee2e6b4c1ee42523bf42efa7
thread-koder <mohamad.fberjawi@gmail.com>
2022-03-27 16:37:49 +0300

    Create the mehodology page basic template

---
+++ pages/methodology/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Hero title="Methodology" />
+</template>
+
+<script>
+export default {
+  name: 'MethodologyPage',
+  components: {
+    Hero: () => import('~/components/Hero.vue'),
+  },
+}
+</script>
+
+<style lang="postcss" scoped></style>
