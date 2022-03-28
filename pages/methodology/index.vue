9eb9c2a928fa9f161bc320460e00a6d3a1254b5b
thread-koder <mohamad.fberjawi@gmail.com>
2022-03-28 13:48:00 +0300

    Add the header text for the methodology page

--- pages/methodology/index.vue
+++ pages/methodology/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <Hero title="Methodology" />
+  <div class="page-container">
+    <Hero title="Contributors" />
+    <div class="header-text">
+      <div class="divider-black"></div>
+      <h2>About JOSA</h2>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,4 +17,27 @@ export default {
 }
 </script>
 
-<style lang="postcss" scoped></style>
+<style lang="postcss" scoped>
+.page-container {
+  @apply bg-gray;
+}
+
+.top-developers {
+  @apply 2xl:mx-3;
+}
+
+.flex-container {
+  @apply items-center md:flex md:justify-between;
+}
+
+.divider-black {
+  @apply w-10 lg:w-10 border-t-2 border-black;
+}
+
+.header-text {
+  font-family: 'IBM Mono';
+  font-size: 1.7rem;
+  line-height: 1em;
+  @apply font-normal mx-7 pb-4 pt-16 md:mx-12 lg:mx-24;
+}
+</style>
