dbc66df04083fd5649aa7b1092f4408dbdcd1843
thread-koder <mohamad.fberjawi@gmail.com>
2022-08-24 19:04:02 +0300

    Modify tailwind.config to work with tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  purge: [
+  content: [
     './components/**/*.{vue,js}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       height: {
@@ -21,9 +20,6 @@ module.exports = {
         },
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
