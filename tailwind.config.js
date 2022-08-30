a16f974446ac157c4be24949314b3d371bc53e99
thread-koder <mohamad.fberjawi@gmail.com>
2022-08-30 14:02:52 +0300

    Modify tailwind config file to work with the new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  purge: [
+  content: [
     './components/**/*.{vue,js}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       height: {
