648b3c69120025594c640dadf320a3e859fcbf4b
thread-koder <mohamad.fberjawi@gmail.com>
2023-01-08 12:43:38 +0300

    Add LastUpdated to the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="bg-gray">
     <Hero />
+    <LastUpdated />
     <LazyHydrate when-visible>
       <Stats />
     </LazyHydrate>
@@ -13,12 +14,14 @@
 <script>
 import LazyHydrate from 'vue-lazy-hydration'
 import Hero from '../components/HomePageHero.vue'
+import LastUpdated from '~/components/LastUpdated.vue'
 export default {
   components: {
     Hero,
     Stats: () => import('../components/Stats.vue'),
     TopDevelopers: () => import('../components/TopDevelopers.vue'),
     LazyHydrate,
+    LastUpdated,
   },
   mounted() {
     this.$store.commit('setPeriod', 'last_30_days')
